4127be6b55c6a5fa5dceb9767f3ee357c6267b64
upadhyayaditi <au.aditi08@gmail.com>
2024-02-29 11:54:15 +0530

    modified Basic Array list

--- Arrays and ArrayLists/BasicArrayList.java
+++ Arrays and ArrayLists/BasicArrayList.java
@@ -2,10 +2,9 @@ import java.util.*;
 
 public class BasicArrayList {
     public static void main(String[] args) {
-        
 
         // Create an ArrayList of integers with an initial capacity of 5
-        ArrayList<Integer> list = new ArrayList<>(5); 
+        ArrayList<Integer> list = new ArrayList<>(5);
 
         // // Add elements to the ArrayList
         list.add(67);
@@ -14,8 +13,10 @@ public class BasicArrayList {
         list.add(3477428);
         list.add(2462);
 
-        // // Attempt to add another element to the ArrayList, which will exceed the initial capacity
-        // // This will automatically resize the ArrayList to accommodate the new element
+        // // Attempt to add another element to the ArrayList, which will exceed the
+        // initial capacity
+        // // This will automatically resize the ArrayList to accommodate the new
+        // element
         list.add(6467);
 
         // Print the ArrayList
