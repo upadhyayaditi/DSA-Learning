4d0a27c6055399dc6543aee2aad70d12b574b828
upadhyayaditi <au.aditi08@gmail.com>
2024-03-09 11:47:06 +0530

    Linked list Built-in class program added

---
+++ Linked List/LinkedListClass.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class LinkedListClass {
+    public static void main(String[] args) {
+        // Creating a LinkedList object to store integers
+        List<Integer> list = new LinkedList<>();
+
+        // Adding elements to the LinkedList
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        // Printing the contents of the LinkedList
+        System.out.println(list);
+    }
+}
